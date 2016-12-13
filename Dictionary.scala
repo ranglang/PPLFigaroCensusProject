ae1345dbd6406b100ecc72d01ef9c19c9bb03707
Yennie Jun <Yennie.jun@gmail.com>
2016-12-13 17:57:45 -0500

    Commented the dictionary

--- Dictionary.scala
+++ Dictionary.scala
@@ -5,27 +5,21 @@ import java.nio.file.{Files,Paths,Path}
 import java.io._
 import scala.io.Source
 
-class Dictionary(totalPopulationState: Int) {
-  val statename: String = ""
+class Dictionary(totalPopulationState: Int, stateNameParam: String) {
+
+  // These are member variables of the Dictionary class 
+  val statename: String = stateNameParam
   val totalPopulation = totalPopulationState
 
-  // do we even need a master list of labels...?
-  // prolly not if we have ageList + raceList + etc
-  // well i'll just keep one now for funsies!!!!
+  // Master list of labels (all of them)
   val labels: ListBuffer [String] = ListBuffer()
-  val dependencies: ListBuffer[String] = ListBuffer()
+  //val dependencies: ListBuffer[String] = ListBuffer()
 
   val ageList: ListBuffer[String] = ListBuffer()
   val raceList: ListBuffer[String] = ListBuffer()
   // can have other lists here too .... 
+  // I just have age and race for now to keep things simple
 
-
-
-
-// Map[population, (isFemale, [metadata list])]
-  //val dependencies : Map[Int, (Boolean, ListBuffer[String])] 
-  // TODO: Add here: addMetadataLabelshit
-  // need to store all of the metadata stuff here
 
   def addLabel(labelName: String) 
   {
@@ -41,61 +35,54 @@ class Dictionary(totalPopulationState: Int) {
     raceList += raceLabel
     addLabel(raceLabel)
   }
+
   //etc
 
-  def addDependencies(metadata: String) {
-    dependencies += metadata
-    // how to add each label in the metadata and make the connections?
-  }
-
   }
 
 
-
-// TODO: Make a class for Dependency and for Label
 object Dictionary {
-  val totalPopMass = 6547629
 
-  // Returns the dictionary with all of the labels..?
+  // some extra information
+  val totalPopMass = 6547629
+  val stateName = "Massachusetts"
+
+  // returns the Dictionary class given a list of ageLabels and a list of raceLabels 
   def fromParams(ageLabels: Traversable[String], raceLabels: Traversable[String]) = {
-    val result = new Dictionary(totalPopMass)
+    val result = new Dictionary(totalPopMass, stateName)
     for { agelabel <- ageLabels } { result.addAgeLabel(agelabel) }
     for { racelabel <- raceLabels} { result.addRaceLabel(racelabel) }
     result
   }
 
-  // RETURNS a string list
-  // i.e. the list of all of the race params 
-  // or all of the age params
-  // or all of the params in the given file 
+  // Reads in all of the parameters for a category given the file. Returns a string List (actually, a listBuffer) of each label 
   def readParams(fileName : String) = {
     val source = Source.fromFile(fileName)
     val result = new ListBuffer [String]()
     for {
       line <- source.getLines()
     } {
-      // each line is a new param label
+      // each line is a new label
       result += line 
     }
     result 
   }
 
+
   def main(args: Array[String]) = {
     // NOTE: these param files need to be in the format:
-    // "white" "black" "asian" "something else" 
-    // all on the same line, separated by a NEWLINE 
+    // "white" "black" "asian" "something else" separated by a NEWLINE 
     val ageParams = readParams("data/params/age.txt")
     val raceParams = readParams("data/params/race.txt")
     val dict = Dictionary.fromParams(ageParams, raceParams)
 
-    println("Total number of age parameters: " + dict.ageList.length)
+    println("\nTotal number of age parameters: " + dict.ageList.length)
     dict.ageList.foreach(println)
 
     println("\nTotal number of race parameters: " + dict.raceList.length)
     dict.raceList.foreach(println)
 
     println("\nAll parameters: " + dict.labels.length)
-    dict.labels.foreach(println) //print all the labels! Woo!
+    dict.labels.foreach(println) 
   }
-
 }
